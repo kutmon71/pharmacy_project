4ed78daa7f89f5c740ca4ddd9657051eb7d15471
kutmon71 <kutman.maratov@alatoo.edu.kg>
2023-07-14 14:45:25 +0600

    add Information and Logout function

--- code.cpp
+++ code.cpp
@@ -484,10 +484,116 @@ void DeliverMedicine(){
    }while(deliverMoreChoice == 'y' || deliverMoreChoice == 'Y');
 }
 
+void Information(){
+   cout << R"(
+      "Pharmacy automation".
+
+The pharmacy automation program, to help pharmaceutical company employees and those who work in these companies, is designed to improve conditions for pharmacy workers, suppliers and customers. The program performs automatic calculations and calculations, automatically determines the shortage of a particular drug in the pharmacy. 
+
+The users of the program are a pharmacist, a supplier. To access the program, you need to log in to your account using your username and password.
+
+Note:
+- In the files, the information is given in the format "Name of the medicine, price, quantity, time".
+   )";
+}
+
+void Logout(){
+   cout << "The program is completed, we will be glad to see you back! \n";
+}
+
 int main()
 {
    Login();
-   Menu();
+   int OperationSelection;
+   cin >> OperationSelection;
+   do
+   {
+      if (AccountType == "Farmworker")
+      {
+         cout << R"(
+            Greetings dear Pharmacist! Please dial the menu number to work with the program, if finished, then dial 8:
+
+         1. Search for medicines
+         2. Show the full list of medications
+         3. Change the price for the medicine
+         4. Sell the medicine
+         5. Show the stock of medicines
+         6. Order medicine
+         7. Show information about the program 
+
+         0. Exit
+         )";
+
+         switch (OperationSelection)
+         {
+         case 1:
+            Search();
+            break;
+         case 2:
+            ListOfMedications();
+            break;
+         case 3:
+            ChangePrice();
+            break;
+         case 4:
+            Sell();
+            break;
+         case 5:
+            Stock();
+            break;
+         case 6:
+            Order();
+            break;
+         case 7:
+            Information();
+            break;
+         case 0:
+            break;
+         default:
+            cout << "Please choose from the options provided.";
+            break;
+         }
+      }
+      else
+      { // that means AccountType is Supplier
+         cout << R"(
+            Greetings dear Supplier! Please dial the menu number to work with the program, if finished, then dial 6:
+
+         1. Show a list of the entire list of medicines from pharmacies
+         2. Show the medicine required for delivery
+         3. Deliver the medicine
+         4. Show the delivered medicines
+         5. Show information about the program
+
+         0. Exit
+         )";
+
+         switch (OperationSelection)
+         {
+         case 1:
+            ListOfMedications();
+            break;
+         case 2:
+            MedicineForDelivery();
+            break;
+         case 3:
+            DeliverMedicine();
+            break;
+         case 4:
+            DeliveredMedicines();
+            break;
+         case 5:
+            Information();
+            break;
+         case 0:
+            break;
+         default:
+            cout << "Please choose from the options provided.";
+            break;
+         }
+      }
+   } while (OperationSelection != 0);
+
    Logout();
    return 0;
 }
